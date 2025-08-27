993ef9345d140f78b1fef809a9f35074a55871bb
Anubhaw-Singh <vkt94594@gmail.com>
2025-08-27 23:58:19 +0530

    Added Sorting functionality on price and Finished the project

--- amazing-animal-paintings/src/app/product/product-list/product-list.component.ts
+++ amazing-animal-paintings/src/app/product/product-list/product-list.component.ts
@@ -10,21 +10,22 @@ import { Router } from '@angular/router';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
-  products:Product[]=[];
-  filteredProducts:Product[]=[];
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  sortOrder: string = "";   // this will be used for sorting before and after filter
   constructor(private productService: ProductService,
-    private cartService: CartService, private snackBar: MatSnackBar, private router:Router) { }
+    private cartService: CartService, private snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
-     this.productService.getProducts().subscribe(data=>{
-            this.products = data;
-            this.filteredProducts = data;
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+      this.filteredProducts = data;
     })
   }
-  addToCartSnackBar(product: Product){
+  addToCartSnackBar(product: Product) {
     this.cartService.addToCart(product).subscribe({
-      next: ()=>{
+      next: () => {
         this.snackBar.open(
           `${product.name} added to cart !`,
           'View Cart',
@@ -32,13 +33,13 @@ export class ProductListComponent implements OnInit{
             duration: 4000,
             horizontalPosition: 'right',
             verticalPosition: 'top',
-            
+
           }
-        ).onAction().subscribe(()=>{
-            this.router.navigate(['/cart'])
+        ).onAction().subscribe(() => {
+          this.router.navigate(['/cart'])
         })
       },
-      error: ()=>{
+      error: () => {
         this.snackBar.open(
           `Failed to add ${product.name} to cart!!`,
           'Dismiss',
@@ -52,12 +53,24 @@ export class ProductListComponent implements OnInit{
     });
   }
 
-  applyFilter(event:Event){
-    let searchTerm= (event.target as HTMLInputElement).value.toLowerCase();
-    this.filteredProducts = this.products.filter((product)=>{
+  applyFilter(event: Event) {
+    let searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredProducts = this.products.filter((product) => {
       return product.name.toLowerCase().includes(searchTerm);
     })
+    // after searching/filtering we also want to sort
+    this.sortProducts(this.sortOrder);
 
+  }
+
+  sortProducts(sortValue: string) {
+    this.sortOrder = sortValue;
+    if (this.sortOrder === "priceLowHigh") {
+      this.filteredProducts.sort((a, b) => a.price - b.price)
+    }
+    else if (this.sortOrder === "priceHighLow") {
+      this.filteredProducts.sort((a, b) => b.price - a.price)
+    }
   }
 
 
